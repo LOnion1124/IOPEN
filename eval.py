957e0faee99b00376d5097f74b3737d7607afb5f
LOnion1124 <2489896340@qq.com>
2026-03-15 17:49:47 +0800

    adapt to new data preprocess method

--- eval.py
+++ eval.py
@@ -1,4 +1,8 @@
 from src.eval import make_evaluator
+from src.datasets import make_dataloader
+
+dataloader = make_dataloader()
+batch = next(iter(dataloader))
 
 evaluator = make_evaluator()
-evaluator.evaluate()
+evaluator.evaluate(batch)
--- train.py
+++ train.py
@@ -5,8 +5,9 @@ train_cfg = cfg['train']
 
 trainer = make_trainer(
     epochs=train_cfg['epoch'],
-    log_interval=10,
+    log_interval=50,
     ckpt_dir=train_cfg['result_path'],
-    save_interval=1
+    save_interval=25,
+    use_adaptive_weight=train_cfg['use_adaptive_weight']
     )
 trainer.train()
