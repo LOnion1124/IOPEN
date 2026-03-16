78505f161fec05fffddfdaad83d9b090a6b9e2c8
LOnion1124 <2489896340@qq.com>
2026-03-16 23:59:29 +0800

    update train wrapper

--- train.py
+++ train.py
@@ -5,9 +5,14 @@ train_cfg = cfg['train']
 
 trainer = make_trainer(
     epochs=train_cfg['epoch'],
-    log_interval=50,
+    log_interval=train_cfg['log_interval'],
     ckpt_dir=train_cfg['result_path'],
-    save_interval=25,
+    save_interval=train_cfg['save_interval'],
     use_adaptive_weight=train_cfg['use_adaptive_weight']
     )
+
+if train_cfg['use_checkpoint']:
+    trainer.load_checkpoint(train_cfg['checkpoint_path'])
+    print(f"Loaded {train_cfg['checkpoint_path']}")
+
 trainer.train()
