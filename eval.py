a14dd47e75e17e5004d34521684f1fd1552aa671
LOnion1124 <2489896340@qq.com>
2026-04-02 21:44:31 +0800

    use validation set for batch mode

--- eval.py
+++ eval.py
@@ -6,7 +6,8 @@ eval_mode = cfg['eval']['mode']
 evaluator = make_evaluator()
 
 if eval_mode == 'batch':
-    dataloader = make_dataloader()
+    val_batch_size = cfg['train'].get('val_batch_size', cfg['train']['batch_size'])
+    dataloader = make_dataloader(split='validate', shuffle=False, batch_size=val_batch_size)
     batch = next(iter(dataloader))
     evaluator.evaluate(mode='batch', batch=batch)
 elif eval_mode == 'coco':
